a187a9b1bcbfef7ecd7f2a54ce1f359350f2a251
smookees <bernatribaruiz@gmail.com>
2022-11-14 13:22:49 +0100

    Act 03 Bernat Riba

---
+++ activitats/act03/helpers.c
@@ -0,0 +1,11 @@
+#include "helpers.h"
+#include <string.h>
+#include <stdio.h>
+
+int split_line(char *line, char* separator, char ***tokens) {
+
+}
+
+int read_line(char** line) {
+    
+}
--- activitats/act03/main.c
+++ activitats/act03/main.c
@@ -1,8 +1,84 @@
+#include <stdio.h>
+#include "helpers.h"
+#include <dirent.h>
+#include <stdlib.h>
 
-int
-main(int argc, char *argv[]){
-    // while not comand exit
-        // readline
-        // parseline
-        // launch comand
+#define SOTEXT ">ossh "
+void functionChoose(char token[], char command[]);
+
+int main(int argc, char *argv[]){
+    char command[100];
+
+    printf("%s", &SOTEXT);
+    scanf("%[^\n]", &command);
+    fflush(stdin);
+    
+    while (strcmp(command, "exit") != 0) {
+        char *token = strtok(command, ";");
+
+        while (token != NULL) {
+            if (strstr(token, "<") != NULL || strstr(token, ">") != NULL || strstr(token, "|") != NULL || strstr(token, "&") != NULL ) {
+                printf("Not allowed operators [|><&] \n");
+            } else {
+                printf("%s", &token);
+                functionChoose(token, command);
+            }
+            token = strtok(NULL, ";");
+        }
+
+        printf("%s", &SOTEXT);
+        scanf("%[^\n]", &command);
+        fflush(stdin);
+    }
+}
+
+void functionChoose(char token[], char commands[]) {
+
+    if (strcmp(token, "ls") == 0) 
+    {
+        DIR *d = opendir(".");
+        struct dirent *dentry;
+        size_t i=1;
+ 
+        while((dentry=readdir(d))!=NULL)
+        {
+            printf("%u. %s\n", i, dentry->d_name);
+        }
+    } 
+
+    else if (strcmp(token, "pwd") == 0)
+    {
+        char path[200];
+
+        getcwd(path, 200);
+        printf("%s\n", path);
+    }
+    
+    else if (strcmp(token, "echo") == 0)
+    {
+        int found = 0;
+
+        for (int i = 0; i < sizeof(&commands); i++) {
+            if (&commands[i] == "\"" && found == 0) {
+                found = 1;
+                printf("\"");
+            } else if (&commands[i] == "\"" && found == 1) {
+                found = 0;
+                printf("\"");
+            } else if (found == 1) {
+                printf("%c", commands[i]);
+            }
+        }  
+        printf("\n"); 
+    } 
+
+    else if (strcmp(token, "help") == 0)
+    {
+        printf("#### GTIDIC - UDL - OSSH ##################################\n");
+        printf("#                                                         #\n");
+        printf("# Type program names and arguments, and hit enter.        #\n");
+        printf("# Use the man command for information on other programs.  #\n");
+        printf("#                                                         #\n");
+        printf("###########################################################\n");
+    } 
 }
